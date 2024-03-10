6820538b1f416b1f8f3232d61df01d22ead61a10
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-11 04:22:42 +0530

    Add files via upload

---
+++ 01-practice.js
@@ -0,0 +1,20 @@
+// Given a string, reverse each word in the sentence.
+
+/*var str = "Welcome to this Javascript Guide!";
+
+var savedstr = str.split(" ").map(function(e){
+    return e.split("").reverse().join("")
+})
+
+console.log(savedstr.join(" "))*/
+
+// console.log(str.split("").reverse().join())
+
+
+var word = "hello my name tushar gupta"
+
+var str = word.split(" ").map(function(e){
+    return e.split("").reverse().join("")
+})
+
+console.log(str.join(" "))
---
+++ 02-practice.js
@@ -0,0 +1,14 @@
+// How to check if an object is an array or not?
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(typeof arr);
+
+
+function checkArray(e){
+    return Array.isArray(e);
+    // console.log(Array.isArray(e)); 
+}
+
+console.log(checkArray([]));
+console.log(checkArray({}));
