7d17cf45f3ea17cd3cef2c9bb5364516e298e8ed
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-21 16:50:25 +0530

    Update

--- 08-practice.js
+++ 08-practice.js
@@ -1,10 +1,15 @@
-function university(shortname, year, student){
-    this.shortname = shortname,
-    this.year = year,
-    this.student = student
-}
+// function university(shortname, year, student){
+//     this.shortname = shortname,
+//     this.year = year,
+//     this.student = student
+// }
 
 
-const university1 = new university('cu', 2001, 85000)
+// const university1 = new university('cu', 2001, 85000)
 
-console.log(university1.shortname)
+// console.log(university1.shortname)
+
+
+
+const muNam = '_tushar'
+console.log('nuNam');
