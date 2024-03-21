dfa3b60129e96bbf4db70d3f3166c4314553e82d
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-21 13:06:21 +0000

    User Input

---
+++ 09-practice.js
@@ -0,0 +1,22 @@
+const a = '';
+const b = '';
+
+var input = function(){
+    return Number(prompt(`Enter the value of A : ${a}`));
+}
+
+var input2 = function(){
+    return Number(prompt(`Enter the value of B : ${b}`));
+}
+
+let userInputOne = input();
+let userInputTwo = input2();
+
+
+if(userInputOne < userInputTwo){
+    console.log(`${userInputOne} is less then ${userInputTwo}`)
+} else if (userInputOne > userInputTwo){
+    console.log(`${userInputOne} is greater then ${userInputTwo}`)
+} else{
+    console.log(`${userInputOne} both are equal ${userInputTwo}`)
+}
