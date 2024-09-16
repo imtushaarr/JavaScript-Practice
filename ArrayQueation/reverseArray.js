6d146b29b0b945c4db7565c4706e259b1e3e7334
Tushar Gupta <tusharguptagps@gmail.com>
2024-09-16 01:27:21 +0000

    Solved

---
+++ ArrayQueation/reverseArray.js
@@ -0,0 +1,9 @@
+// Reverse an Array
+// Problem: Write a function that takes an array and returns a new array with the elements in reverse order
+
+
+let arr = [1, 2, 3, 4]
+
+let result = arr.reverse();
+console.log(result);
+
