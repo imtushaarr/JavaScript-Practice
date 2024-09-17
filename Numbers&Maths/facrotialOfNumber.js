c0f904aabc4ed199602630c0293127f0e611b04f
Tushar Gupta <tusharguptagps@gmail.com>
2024-09-17 19:47:01 +0530

    Create facrotialOfNumber.js

---
+++ Numbers&Maths/facrotialOfNumber.js
@@ -0,0 +1,18 @@
+// Find the Factorial of a Number
+// Write a function that takes a number as input and returns its factorial.
+
+let num =  5;
+// Output: 120
+
+function facrotialOfNumber(num){
+    if(num === 0 || num === null) return 1;
+
+    let result = 1;
+
+    for(i=1; i <= num; i++){
+        result *= i;
+        }
+    return result;
+}
+
+console.log(facrotialOfNumber(num))
