468435819b0edfd7d7fbadbcf856b059cd9c3bc9
Tushar Gupta <tusharguptagps@gmail.com>
2024-09-19 11:12:26 +0530

    sumOfNumbers

--- Numbers&Maths/sumOfNumbers.js
+++ Numbers&Maths/sumOfNumbers.js
@@ -1,1 +1,16 @@
+// Sum of Digits
+// Write a function that takes a number as input and returns the sum of its digits.
 
+const num = 123
+// Output: 6
+
+
+function sumOfNumbers(num){
+    let splitNumber = num.toString().split('').map(Number);
+
+    let sum = splitNumber.reduce((pre, cur) => pre+ cur);
+
+    return sum;
+}
+
+console.log(sumOfNumbers(num));
